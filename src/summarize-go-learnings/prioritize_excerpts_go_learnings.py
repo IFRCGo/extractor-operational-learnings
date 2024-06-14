81571880fb448b39ca7cbf57d8ab8851d3e87a2c
ypyelab <37597139+ypyelab@users.noreply.github.com>
2024-06-14 17:18:43 +0200

    add secondary summary pipeline

--- src/summarize-go-learnings/prioritize_excerpts_go_learnings.py
+++ src/summarize-go-learnings/prioritize_excerpts_go_learnings.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import tiktoken
 import sys
+from itertools import chain
 import logging
 
 
@@ -12,9 +13,15 @@ ENCODING_NAME = "cl100k_base"
 logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
 
 
-def remove_duplicates(df):
-    """Remove duplicate rows based on the 'learning' column."""
-    return df.drop_duplicates(subset='learning')
+def remove_duplicates(df, type_prompt):
+    """Remove duplicate rows based on the 'learning' and 'component' column."""
+    if type_prompt == 'primary':
+        return df.drop_duplicates(subset='learning')
+    elif type_prompt == 'secondary':
+        return df.drop_duplicates(subset=['learning','component'])
+    else:
+        logging.error('Type of prompt is not valid. Type has to be either primary or secondary.')
+        return None
 
 
 def count_tokens(string, encoding_name):
@@ -23,9 +30,26 @@ def count_tokens(string, encoding_name):
     return len(encoding.encode(string))
 
 
-def sort_excerpts(df):
+def sort_excerpts(df, type_prompt):
     """Sort DataFrame by 'appeal_year' in descending order."""
-    return df.sort_values(by='appeal_year', ascending=False)
+    if type_prompt == 'primary':
+        df = df.sort_values(by='appeal_year', ascending=False)
+        df.reset_index(inplace = True, drop = True)
+        return df
+    elif type_prompt == 'secondary':
+        df_sorted = df.sort_values(by=['component', 'appeal_year'], ascending=[True, False])
+        grouped = df_sorted.groupby('component')
+
+        # Create an interleaved list of rows
+        interleaved = list(chain(*zip(*[group[1].itertuples(index=False) for group in grouped])))
+
+        # Convert the interleaved list of rows back to a DataFrame
+        result = pd.DataFrame(interleaved)
+        result.reset_index(inplace = True, drop = True)
+        return result
+    else:
+        logging.error('Type of prompt is not valid. Type has to be either primary or secondary.')
+        return None
 
 
 def slice_dataframe(df, limit, encoding_name):
@@ -42,37 +66,38 @@ def slice_dataframe(df, limit, encoding_name):
         df_sliced = df.iloc[:slice_index+1]
                 
     else:
-        df_sliced = df 
-            
+        df_sliced = df    
     return df_sliced
 
 
-def prioritize_most_recent(df, limit=2000, encoding_name="cl100k_base"):
+def prioritize_most_recent(df, type_prompt, limit=2000, encoding_name="cl100k_base"):
     """Prioritize the most recent excerpts within the token limit."""
-    df = remove_duplicates(df)
-    df = sort_excerpts(df)
+    df = remove_duplicates(df, type_prompt)
+    df = sort_excerpts(df, type_prompt)
     return slice_dataframe(df, limit, encoding_name)
     
 
-def prioritize_excerpts(contextualized_learnings):     
+def prioritize_excerpts(contextualized_learnings, type_prompt):     
     """Main function to prioritize excerpts.""" 
     prioritized_excerpts_learnings = prioritize_most_recent(
         contextualized_learnings, 
+        type_prompt,
         limit=PROMPT_DATA_LENGTH_LIMIT, 
-        encoding_name=ENCODING_NAME
+        encoding_name=ENCODING_NAME,
     )
     logging.info("Prioritization of learnings completed.")
     return prioritized_excerpts_learnings
 
 
-def main(contextualized_learnings):
-    return prioritize_excerpts(contextualized_learnings)
+def main(contextualized_learnings, type_prompt):
+    return prioritize_excerpts(contextualized_learnings, type_prompt)
 
   
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python prioritize_excerpts_go_learnings.py contextualized_learnings")
+    if len(sys.argv) != 3:
+        print("Usage: python prioritize_excerpts_go_learnings.py contextualized_learnings type_prompt")
     else:
         contextualized_learnings = sys.argv[1]
-        prioritized_learnings = main(contextualized_learnings)
+        type_prompt = sys.argv[2]
+        prioritized_learnings = main(contextualized_learnings, type_prompt)
         print(prioritized_learnings)
