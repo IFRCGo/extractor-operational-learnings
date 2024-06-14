0072e16f5c19fc5b0e687fa709936e409d89f64b
ypyelab <37597139+ypyelab@users.noreply.github.com>
2024-06-14 17:19:16 +0200

    add secondary summary pipeline

--- src/summarize-go-learnings/format_prompt_go_learnings.py
+++ src/summarize-go-learnings/format_prompt_go_learnings.py
@@ -2,10 +2,14 @@ import pandas as pd
 import json
 import sys
 import os
+import tiktoken
 import logging
 
 
-FORMAT_PROMPT_PATH = "format_prompt.txt"
+FORMAT_PROMPT_PRIMARY_PATH = "format_prompt.txt"
+FORMAT_PROMPT_SECONDARY_PATH = "format_prompt_secondary.txt"
+PROMPT_DATA_LENGTH_LIMIT = 5000
+ENCODING_NAME = "cl100k_base"
 
 
 def read_json_file(file_path):
@@ -36,10 +40,98 @@ def read_file(file_path):
         raise
 
 
+def count_tokens(string, encoding_name):
+    """Returns the number of tokens in a text string."""
+    encoding = tiktoken.get_encoding(encoding_name)
+    return len(encoding.encode(string))
+
+
+def slice_dataframe(df, limit, encoding_name):
+    df['count_temp'] = [count_tokens(x,encoding_name) for x in df['learning']]
+    df['cumsum'] = df['count_temp'].cumsum()
+
+    slice_index = None
+    for i in range(1, len(df)):
+        if df['cumsum'].iloc[i-1] <= limit and df['cumsum'].iloc[i] > limit:
+            slice_index = i-1
+            break
+
+    if slice_index is not None:
+        df_sliced = df.iloc[:slice_index+1]
+                
+    else:
+        df_sliced = df    
+    return df_sliced
+
+
 def process_request_filter(request_filter):
     """Removes keys with empty values from the request filter."""
     request_filter = {k: v for k, v in request_filter.items() if v}
     return request_filter
+
+
+def get_main_sectors(df):
+    """Get only information from technical sectorial information"""
+    temp = df[df['component']=='NS-specific areas of intervention']
+    available_sectors= list(temp['sector'].unique())
+    nb_sectors = len(available_sectors)
+    if nb_sectors == 0:
+        logging.info("There were not specific technical sectorial learnings")
+        return []
+    else:
+        logging.info("Main sectors for secondary summaries selected")
+        return available_sectors
+
+
+def get_main_components(df):
+    available_components= list(df['component'].unique())
+    nb_components= len(available_components)
+    if nb_components == 0:
+        logging.info("There were not specific components")
+        return []
+    else:
+        logging.info("All components for secondary summaries selected")
+        return available_components
+
+
+def get_learnings_sector(sector, df):
+    return df[df['sector']==sector]
+
+
+def get_learnings_component(component, df):
+    return df[df['component']==component]
+
+
+def process_learnings_sector(sector,df, max_length_per_section):
+    df = get_learnings_sector(sector, df).dropna()
+    df_sliced = slice_dataframe(df, max_length_per_section, ENCODING_NAME)
+    learnings_sector = '\n----------------\n'+"SUBTYPE: " + sector+'\n----------------\n'+'\n----------------\n'.join(df_sliced['learning'])
+    return learnings_sector
+
+
+def process_learnings_component(component,df, max_length_per_section):
+    df = get_learnings_component(component, df).dropna()
+    df_sliced = slice_dataframe(df, max_length_per_section, ENCODING_NAME)
+    learnings_component = '\n----------------\n'+"SUBTYPE: " + component+'\n----------------\n'+'\n----------------\n'.join(df_sliced['learning'])
+    return learnings_component
+
+
+def process_data(type_prompt, df):
+    """Process learnings from DataFrame according to type of summary"""
+    if (type_prompt == "primary"):
+        learnings_data = '\n----------------\n'.join(df['learning'].dropna())
+        return learnings_data
+    elif (type_prompt == "secondary"):
+        sectors = get_main_sectors(df)
+        components = get_main_components(df)
+        max_length_per_section = PROMPT_DATA_LENGTH_LIMIT / (len(components)+len(sectors))
+        learnings_sectors = '\n----------------\n\n'+"TYPE: SECTORS"+'\n----------------\n'.join([process_learnings_sector(x,df, max_length_per_section) for x in sectors if pd.notna(x)])
+        learnings_components = '\n----------------\n\n'+"TYPE: COMPONENT"+'\n----------------\n'.join([process_learnings_component(x,df, max_length_per_section) for x in components if pd.notna(x)])
+        learnings_data = learnings_sectors + learnings_components
+
+        return learnings_data
+    else:
+        logging.error("Type of prompt is not valid. Type has to be either primary or secondary.")
 
 
 def build_intro_section():
@@ -77,23 +169,32 @@ def build_instruction_section(request_filter, df):
         instructions.append(f'and "{component_str}" aspects')
 
     instructions.append(
-        'in Emergency Response. You should prioritize the insights based on their recurrence and potential impact on humanitarian operations, and provide the top 3 insights. Describe step by step your thought process.\n\nI will pass you the DATA section, are you ready?\n\n'
+        'in Emergency Response. You should prioritize the insights based on their recurrence and potential impact on humanitarian operations, and provide the top insights. \n\nI will pass you the DATA section, are you ready?\n\n'
     )
 
     return '\n'.join(instructions)
 
 
-def build_data_section(df):
+def build_data_section(type_prompt, df):
     """Builds the data section of the prompt from the DataFrame."""
-    learnings_data = '\n----------------\n'.join(df['learning'].dropna())
-    return f'DATA\n========================\n{learnings_data}\n\nI will pass you the FORMAT section, are you ready?\n\n'
+    try:
+        learnings_data = process_data(type_prompt,df)
+        return f'DATA\n========================\n{learnings_data}\n\nI will pass you the FORMAT section, are you ready?\n\n'
+    except Exception as e:
+        logging.error(f"Error in generating summaries: {e}")
+        raise
 
-
-def get_format_section():
+def get_format_section(type_prompt):
     """Reads the format section from a text file."""
     try:
-        content= read_file(FORMAT_PROMPT_PATH)
-        return content
+        if (type_prompt == "primary"):
+            content= read_file(FORMAT_PROMPT_PRIMARY_PATH)
+            return content
+        elif (type_prompt == "secondary"):
+            content= read_file(FORMAT_PROMPT_SECONDARY_PATH)
+            return content
+        else:
+            logging.error("Type of prompt is not valid. Type has to be either primary or secondary.")
     except Exception as e:
         logging.error(f"Error in generating summaries: {e}")
         raise
@@ -104,32 +205,34 @@ def create_prompt(prompt_intro, prompt_instruction, prompt_data, prompt_format):
     return ''.join([prompt_intro, prompt_instruction, prompt_data, prompt_format])
 
 
-def format_prompt(request_filter_path, prioritized_learnings):
+def format_prompt(request_filter_path, prioritized_learnings, type_prompt):
     """Formats the prompt based on request filter and prioritized learnings."""
+
     request_filter = read_json_file(request_filter_path)
     request_filter = process_request_filter(request_filter)
 
     prompt_intro = build_intro_section()
     prompt_instruction = build_instruction_section(request_filter, prioritized_learnings)
-    prompt_data = build_data_section(prioritized_learnings)
-    prompt_format = get_format_section()
+    prompt_data = build_data_section(type_prompt, prioritized_learnings)
+    prompt_format = get_format_section(type_prompt)
 
     return create_prompt(prompt_intro, prompt_instruction, prompt_data, prompt_format)
 
-def main(request_filter_path, prioritized_learnings):
+def main(request_filter_path, prioritized_learnings, type_prompt):
     """Main function to generate the prompt."""
     try:
-        prompt = format_prompt(request_filter_path, prioritized_learnings)
+        prompt = format_prompt(request_filter_path, prioritized_learnings, type_prompt)
     except Exception as e:
         logging.error(f"An error occurred: {e}")
         raise
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: python format_prompt_go_learnings.py request_filter_path prioritized_learnings")
+    if len(sys.argv) != 4:
+        print("Usage: python format_prompt_go_learnings.py request_filter_path prioritized_learnings type_prompt")
     else:
         request_filter_path = sys.argv[1]
         prioritized_excerpts_path = sys.argv[2]
-        prompt = main(request_filter_path, prioritized_learnings)
+        type_prompt = sys.argv[3]
+        prompt = main(request_filter_path, prioritized_learnings, type_prompt)
         print(prompt)
